8c71b9ecdde02f80d835e8f2c58f911d4bbf149c
Alex Bowen <ajbowen249@gmail.com>
2017-08-07 22:53:06 -0400

    working on sandbox with actual program

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -1,31 +1,69 @@
 #include <iostream>
 #include <inMemoryInterface/InMemoryInterface.h>
-#include <nvmCore/Core.h>
+#include <testSupport/ObservableCore.h>
+#include <nvmCore/RegisterTypes.h>
 
 int main(int argc, char** argv) {
     nvm::Interface::Ptr iface(new nvm::InMemoryInterface(1024));
     nvm::Options::Ptr options(new nvm::Options());
-    nvm::Core core;
+    nvm::ObservableCore core;
     core.initialize(iface, options);
     core.reset();
 
-    //Set up for 112 + 58
-    iface->write(0x0000, 0x05);// set literal
-    iface->write(0x0001, 0x10);// ui8-0
-    iface->write(0x0002, 0x70);// value 112
+    /*
+    Set up for something like:
 
-    iface->write(0x0003, 0x05);// set literal
-    iface->write(0x0004, 0x11);// ui8-1
-    iface->write(0x0005, 0x3A);// value 58
+    int j = 0;
+    for(int i = 0; i < 10; i++) {
+        j += 2;
+    }
+    */
 
-    iface->write(0x0006, 0x01);// add
-    iface->write(0x0007, 0x12);// ui8-2
-    iface->write(0x0008, 0x01);// 0|1
+    nvm::address_t address = 0;                                                
+    iface->write(address++, nvm::Instruction::SetLiteral);                //
+    iface->write<int8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x00); //SET i80 0 //j
+    iface->write(address++, 0);                                           //
 
-    //should take exactly 3 process cycles
-    core.process();
-    core.process();
-    core.process();
+    iface->write(address++, nvm::Instruction::SetLiteral);                //
+    iface->write<int8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x01); //SET i81 0 //i
+    iface->write(address++, 0);                                           //
 
-    std::cout << "hello!" << std::endl;
+    iface->write(address++, nvm::Instruction::SetLiteral);                //
+    iface->write<int8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x02); //SET i82 10 //constant for 10
+    iface->write(address++, 10);                                          //
+
+    iface->write(address++, nvm::Instruction::SetLiteral);                //
+    iface->write<int8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x03); //SET i83 2 //constant for 2
+    iface->write(address++, 2);                                           //
+
+    iface->write(address++, nvm::Instruction::SetLiteral);                //
+    iface->write<int8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x04); //SET i84 1 //constant for 1 (this is why we need an increment!)
+    iface->write(address++, 1);                                           //
+                                 
+    auto loopStart = address; //save loop label address
+
+    iface->write(address++, nvm::Instruction::Add);                        //
+    iface->write<uint8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x00); //ADD i80 i80 i83 //j += 2
+    iface->write<uint8_t>(address, 0x03);                                  //
+
+    iface->write(address++, nvm::Instruction::Add);                        //
+    iface->write<uint8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x01); //ADD i81 i81 i84 // i++
+    iface->write<uint8_t>(address, (0x01 << 4) | 0x04);                    //
+
+    iface->write(address++, nvm::Instruction::Subtract);                   //
+    iface->write<uint8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x05); //SUB i85 i81 i82 // test i < 10
+    iface->write<uint8_t>(address, (0x01 << 4) | 0x02);                    //
+
+    iface->write(address++, nvm::Instruction::FixedLiteralJumpNegative);//JFLN loopStart //loop if not less than 10
+    iface->write<nvm::address_t>(address++, loopStart);                 //
+
+    while (core.getInstructionPointer() < address) {
+        std::cout << "address: " << core.getInstructionPointer() << std::endl;
+        core.process();
+    }
+
+    std::cout << "done!" << std::endl;
+    std::cout << "i80: " << (int)core.getI8Register(0) << std::endl;
+    int x;
+    std::cin >> x;
 }
