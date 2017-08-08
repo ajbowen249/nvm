2d7b13f7ec70cbd2fa1dabdb8ea36ea3a9fd5258
ajbowen249 <ajbowen249@gmail.com>
2017-08-08 13:53:21 -0400

    fix sandbox (We have an actual running program!)

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char** argv) {
     }
     */
 
-    nvm::address_t address = 0;                                                
+    nvm::address_t address = 0;
     iface->write(address++, nvm::Instruction::SetLiteral);                //
     iface->write<int8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x00); //SET i80 0 //j
     iface->write(address++, 0);                                           //
@@ -39,31 +39,34 @@ int main(int argc, char** argv) {
     iface->write(address++, nvm::Instruction::SetLiteral);                //
     iface->write<int8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x04); //SET i84 1 //constant for 1 (this is why we need an increment!)
     iface->write(address++, 1);                                           //
-                                 
+
     auto loopStart = address; //save loop label address
 
     iface->write(address++, nvm::Instruction::Add);                        //
     iface->write<uint8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x00); //ADD i80 i80 i83 //j += 2
-    iface->write<uint8_t>(address, 0x03);                                  //
+    iface->write<uint8_t>(address++, 0x03);                                //
 
     iface->write(address++, nvm::Instruction::Add);                        //
     iface->write<uint8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x01); //ADD i81 i81 i84 // i++
-    iface->write<uint8_t>(address, (0x01 << 4) | 0x04);                    //
+    iface->write<uint8_t>(address++, (0x01 << 4) | 0x04);                  //
 
     iface->write(address++, nvm::Instruction::Subtract);                   //
     iface->write<uint8_t>(address++, (nvm::RegisterType::i8 << 4) | 0x05); //SUB i85 i81 i82 // test i < 10
-    iface->write<uint8_t>(address, (0x01 << 4) | 0x02);                    //
+    iface->write<uint8_t>(address++, (0x01 << 4) | 0x02);                  //
 
-    iface->write(address++, nvm::Instruction::FixedLiteralJumpNegative);//JFLN loopStart //loop if not less than 10
-    iface->write<nvm::address_t>(address++, loopStart);                 //
+    iface->write(address++, nvm::Instruction::FixedLiteralJumpNegative); //JFLN loopStart //loop if less than 10
+    iface->write<nvm::address_t>(address++, loopStart);                  //
 
     while (core.getInstructionPointer() < address) {
-        std::cout << "address: " << core.getInstructionPointer() << std::endl;
-        core.process();
+        auto error = core.process();
+        if(error) {
+            std::cout << "Error!" << std::endl;
+            return -1;
+        }
     }
 
     std::cout << "done!" << std::endl;
     std::cout << "i80: " << (int)core.getI8Register(0) << std::endl;
-    int x;
-    std::cin >> x;
+
+    return 0;
 }
