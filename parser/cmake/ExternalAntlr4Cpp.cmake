ba812977302483633b26fc69a9c5482cafb21f98
Alex Bowen <ajbowen249@gmail.com>
2017-08-22 21:18:07 -0400

    windows build junk

--- parser/cmake/ExternalAntlr4Cpp.cmake
+++ parser/cmake/ExternalAntlr4Cpp.cmake
@@ -123,44 +123,47 @@ endif()
 #   endif()
 # endif()
 
-# download runtime environment
-ExternalProject_ADD(
-  #--External-project-name------
-  antlr4cpp
-  #--Depend-on-antrl-tool-----------
-  # DEPENDS antlrtool
-  #--Core-directories-----------
-  PREFIX             ${ANTLR4CPP_EXTERNAL_ROOT}
-  #--Download step--------------
-  GIT_REPOSITORY     ${ANTLR4CPP_EXTERNAL_REPO}
-  # GIT_TAG          ${ANTLR4CPP_EXTERNAL_TAG}
-  TIMEOUT            10
-  LOG_DOWNLOAD       ON
-  #--Update step----------
-  UPDATE_COMMAND     ${GIT_EXECUTABLE} pull
-  #--Patch step----------
-  # PATCH_COMMAND sh -c "cp <SOURCE_DIR>/scripts/CMakeLists.txt <SOURCE_DIR>"
-  #--Configure step-------------
-  CONFIGURE_COMMAND  ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=Release -DANTLR4CPP_JAR_LOCATION=${ANTLR4CPP_JAR_LOCATION} -DBUILD_SHARED_LIBS=ON -BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DCMAKE_SOURCE_DIR:PATH=<SOURCE_DIR>/runtime/Cpp <SOURCE_DIR>/runtime/Cpp
-  LOG_CONFIGURE ON
-  #--Build step-----------------
-  # BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
-  LOG_BUILD ON
-  #--Install step---------------
-  # INSTALL_COMMAND    ""
-  # INSTALL_DIR ${CMAKE_BINARY_DIR}/
-  #--Install step---------------
-  # INSTALL_COMMAND    ""
-)
+if (NOT WIN32)
+	# download runtime environment
+	ExternalProject_ADD(
+	  #--External-project-name------
+	  antlr4cpp
+	  #--Depend-on-antrl-tool-----------
+	  # DEPENDS antlrtool
+	  #--Core-directories-----------
+	  PREFIX             ${ANTLR4CPP_EXTERNAL_ROOT}
+	  #--Download step--------------
+	  GIT_REPOSITORY     ${ANTLR4CPP_EXTERNAL_REPO}
+	  # GIT_TAG          ${ANTLR4CPP_EXTERNAL_TAG}
+	  TIMEOUT            10
+	  LOG_DOWNLOAD       ON
+	  #--Update step----------
+	  UPDATE_COMMAND     ${GIT_EXECUTABLE} pull
+	  #--Patch step----------
+	  # PATCH_COMMAND sh -c "cp <SOURCE_DIR>/scripts/CMakeLists.txt <SOURCE_DIR>"
+	  #--Configure step-------------
+	  CONFIGURE_COMMAND  ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=Release -DANTLR4CPP_JAR_LOCATION=${ANTLR4CPP_JAR_LOCATION} -DBUILD_SHARED_LIBS=ON -BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DCMAKE_SOURCE_DIR:PATH=<SOURCE_DIR>/runtime/Cpp <SOURCE_DIR>/runtime/Cpp
+	  LOG_CONFIGURE ON
+	  #--Build step-----------------
+	  # BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
+	  LOG_BUILD ON
+	  #--Install step---------------
+	  # INSTALL_COMMAND    ""
+	  # INSTALL_DIR ${CMAKE_BINARY_DIR}/
+	  #--Install step---------------
+	  # INSTALL_COMMAND    ""
+	)
 
-ExternalProject_Get_Property(antlr4cpp INSTALL_DIR)
+	ExternalProject_Get_Property(antlr4cpp INSTALL_DIR)
 
-list(APPEND ANTLR4CPP_INCLUDE_DIRS ${INSTALL_DIR}/include/antlr4-runtime)
-foreach(src_path misc atn dfa tree support)
-  list(APPEND ANTLR4CPP_INCLUDE_DIRS ${INSTALL_DIR}/include/antlr4-runtime/${src_path})
-endforeach(src_path)
+	list(APPEND ANTLR4CPP_INCLUDE_DIRS ${INSTALL_DIR}/include/antlr4-runtime)
+	foreach(src_path misc atn dfa tree support)
+	  list(APPEND ANTLR4CPP_INCLUDE_DIRS ${INSTALL_DIR}/include/antlr4-runtime/${src_path})
+	endforeach(src_path)
 
-set(ANTLR4CPP_LIBS "${INSTALL_DIR}/lib")
+	set(ANTLR4CPP_LIBS "${INSTALL_DIR}/lib")
+endif ()
+
 
 # antlr4_shared ${INSTALL_DIR}/lib/libantlr4-runtime.so
 # antlr4_static ${INSTALL_DIR}/lib/libantlr4-runtime.a
